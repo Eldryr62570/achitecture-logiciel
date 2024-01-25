ad13b9d7878275202f8d7c02fea6b9ae1545f925
Pouuub <98018883+Pouuub@users.noreply.github.com>
2024-01-25 09:10:18 +0100

    Delete src directory

--- src/Core/ReflectorWrapper.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Core;
-use ReflectionClass;
-
-class ReflectorWrapper {
-    public function getClassCharacteristics(string $className){
-        $reflectionClass = new ReflectionClass("App\\$className");
-    }
-}
--- src/assets/js/admin.js
+++
@@ -1,8 +0,0 @@
-
-allEditButton.forEach((button) => {
-    button.addEventListener('click' , () =>{
-      showModal("sample-modal2" , 'Modifier un utilisateur')
-        actionInput.value = "changeUser"
-        idUserInput.value= button.dataset.idUser
-    })
-})
